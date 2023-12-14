130d2685b8689c0f5cd2434d6147a4be7e22288d
Koala-123 <138291177+Koala-123@users.noreply.github.com>
2023-12-14 04:41:13 +0000

    Co-authored-by: AaravSahu7 <AaravSahu7@users.noreply.github.com> Co-authored-by: SoumilGhoshDastidar <SoumilGhoshDastidar@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html", title="Hello")
+
+@app.route("/customise")
+def customise():
+    return render_template("customise.html", title="Customise")
+
+@app.route("/focus")
+def focus():
+    return render_template("focus.html", title="Focus Session")
+    
